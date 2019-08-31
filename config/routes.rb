8cbc49468a4eb2f526b80100efc24f038dfad09b
Atsushi <akakak0002@gmail.com>
2019-08-31 15:51:02 +0000

    login ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
   root "static_pages#home"
   get "/about",   to: "static_pages#about"
   get "/help",    to: "static_pages#help"
@@ -9,5 +11,9 @@ Rails.application.routes.draw do
   resources :blogs
   resources :users
 
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
