5c7cabe7d33640f41ce39e38f4b318b6a1b90cc1
Atsushi <akakak0002@gmail.com>
2019-09-11 00:06:23 +0000

    update redirection

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   def create
     blog = Blog.new(blog_params)
     if blog.save
-      redirect_to root_url
+      redirect_to blog
       
     else
       render 'new'
