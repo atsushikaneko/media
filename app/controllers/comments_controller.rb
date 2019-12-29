542ea12f86a70e002413f3d7aaffc2f5002ec6ee
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2019-12-29 19:13:14 +0900

    Fix: commentクラスの属性名を修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,7 +18,7 @@ end
 
 private
   def comment_params
-    params.require(:comment).permit(:comment_content, :blog_id, :user_id)
+    params.require(:comment).permit(:content, :blog_id, :user_id)
   end
 
 end
