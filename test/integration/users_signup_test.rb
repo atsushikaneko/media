f069586138b731537c5c9ae80e187dafdd9e88c0
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2020-01-01 02:27:01 +0900

    Delete: (後々にモーダルにするので)新規登録失敗時のエラーメッセージのテストを削除

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,17 +2,18 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
+  #無効な情報で登録してみる
   test "invalid signup information" do
     get signup_path
+    #無効な譲歩で登録してみて通らないか
     assert_no_difference 'User.count' do
       post users_path, params: { user: { name:  "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
+    # users/newにリダイレクトされるか
     assert_template 'users/new'
-    #assert_select 'div#<CSS id for error explanation>'
-    #assert_select 'div.<CSS class for field with error>'
   end
 
 end
