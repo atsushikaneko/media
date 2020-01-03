34e170d643a7a34d0b06c2c889e583f7bae2914f
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2020-01-03 17:48:50 +0900

    Add: フレンドリーフォワーディングのための各種メソッド追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
+  
   def index
   end
 
@@ -45,4 +48,22 @@ end
 private
 def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+end
+
+
+# beforeアクション
+
+# ログイン済みユーザーかどうか確認
+def logged_in_user
+  unless logged_in?
+    store_location
+    flash[:danger] = "Please log in."
+    redirect_to login_url
+  end
+end
+
+# 正しいユーザーかどうか確認
+def correct_user
+  @user = User.find(params[:id])
+  redirect_to(root_url) unless current_user?(@user)
 end
