08862576e2cf069526194f08bdff679ffd0c9ae2
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2020-01-03 17:46:56 +0900

    Add: フレンドリーフォワーディングの統合テストの追加

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,5 @@
 module SessionsHelper
-  
+
   def remember(user)
     user.remember
     cookies.permanent.signed[:user_id] = user.id
@@ -9,8 +9,8 @@ module SessionsHelper
   def log_in user
   session[:user_id] = user.id
   end
-  
-  
+
+
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -34,10 +34,21 @@ forget(current_user)
 session.delete(:user_id)
 @current_user = nil
 end
-  
+
   def logged_in?
     !current_user.nil?
   end
-  
-  
+
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+   def redirect_back_or(default)
+     redirect_to(session[:forwarding_url] || default)
+     session.delete(:forwarding_url)
+   end
+
+   # アクセスしようとしたURLを覚えておく
+   def store_location
+     session[:forwarding_url] = request.original_url if request.get?
+   end
+
+
 end
