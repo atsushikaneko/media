24c747da5491600d7597de576c7a176d54a82d39
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2020-01-05 20:59:24 +0900

    Add: 各種メソッドの追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,21 @@ class UsersController < ApplicationController
     end
   end
 
+#ユーザーフォロー機能関係
+def following
+  @title = "Following"
+  @user  = User.find(params[:id])
+  @users = @user.following.paginate(page: params[:page])
+  render 'show_follow'
+end
+
+def followers
+  @title = "Followers"
+  @user  = User.find(params[:id])
+  @users = @user.followers.paginate(page: params[:page])
+  render 'show_follow'
+end
+
 
 end
 
