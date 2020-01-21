ce3654311bce1a50dd6b3a0091d4fd55dd20741b
兼子敦史 <atsushi@kanekoatsushinoMacBook-ea.local>
2020-01-21 18:59:23 +0900

    Fix: ルートにfavoriteを追加

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,6 @@ Rails.application.routes.draw do
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :relationships,       only: [:create, :destroy]
-
+  resources :favorites, only: [:create, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
