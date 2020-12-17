56df562d78a0ec6b92becde93ee1876334d5f331
兼子敦史 <atsushi@kanekoatsushinoMacBook-Air.local>
2020-12-17 10:35:01 +0900

    delete: migrationファイルを全削除

--- db/migrate/20190912143852_add_index_to_blog.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToBlog < ActiveRecord::Migration[5.1]
-  def change
-    add_index :blogs, [:user_id, :created_at]
-  end
-end
--- db/migrate/20200115014821_add_profile_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddProfileToUsers < ActiveRecord::Migration[5.1]
-  def change
-    add_column :users, :profile_text, :text
-    add_column :users, :profile_image, :string
-  end
-end
